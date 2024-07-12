2a7de3b647eef2068ddb976b07f43ca9d7520c25
gnagetEmil <emil@gnaget.se>
2024-07-12 14:21:43 +0200

    Added themes.js

---
+++ src/themes.js
@@ -0,0 +1,18 @@
+const lightTheme = {
+    primary: '#7eb455',
+    secondary: '#a3d7b1',
+    background: '#fdfdfc',
+    text: '#0c1208',
+    accent: '#75c29f',
+  };
+  
+  const darkTheme = {
+    primary: '#74aa4b',
+    secondary: '#285c36',
+    background: '#030302',
+    text: '#f1f7ed',
+    accent: '#3d8a67',
+  };
+  
+  export default { light: lightTheme, dark: darkTheme };
+  
