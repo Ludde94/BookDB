de8c9a869f3db8f0b3868bace4d063e2e5676508
gnagetEmil <emil@gnaget.se>
2024-07-13 23:43:38 +0200

    fixed bugged with infinite scrolling

--- src/screens/AddBookScreen/AddBookScreen.js
+++ src/screens/AddBookScreen/AddBookScreen.js
@@ -10,13 +10,13 @@ export default function AddBookScreen({ navigation, route }) {
   const [books, setBooks] = useState([]);
   const [startIndex, setStartIndex] = useState(0);
   const [searchQuery, setSearchQuery] = useState('');
+  const [hasMoreBooks, setHasMoreBooks] = useState(true);  // State to track if there are more books to load
 
-  // Listening to changes in route.params.scannedData
   useEffect(() => {
     if (route.params?.scannedData) {
       const data = route.params.scannedData;
       handleScanResult(data);
-      navigation.setParams({ scannedData: null });  // Resetting scannedData to null to avoid re-triggering on back navigation
+      navigation.setParams({ scannedData: null });
     }
   }, [route.params?.scannedData]);
 
@@ -28,6 +28,9 @@ export default function AddBookScreen({ navigation, route }) {
 
   const fetchData = async (query, startIndex) => {
     const result = await searchBooks(query, startIndex);
+    if (result.length < 10) {  // Assuming the API returns up to 10 books per call
+      setHasMoreBooks(false);  // No more books to load
+    }
     if (startIndex > 0) {
       setBooks(prev => [...prev, ...result]);
     } else {
@@ -38,12 +41,15 @@ export default function AddBookScreen({ navigation, route }) {
   const handleSearch = (query) => {
     setSearchQuery(query);
     setStartIndex(0);
+    setHasMoreBooks(true);  // Reset the pagination and availability of more books
   };
 
   const handleLoadMore = () => {
-    const newIndex = startIndex + 10;
-    fetchData(searchQuery, newIndex);
-    setStartIndex(newIndex);
+    if (hasMoreBooks) {
+      const newIndex = startIndex + 10;
+      fetchData(searchQuery, newIndex);
+      setStartIndex(newIndex);
+    }
   };
 
   const handleScanResult = (data) => {
@@ -52,16 +58,16 @@ export default function AddBookScreen({ navigation, route }) {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-    <View>
-      <SearchBar onSearch={handleSearch} onScan={() => navigation.navigate('Scanner')} />
-      <FlatList
-        data={books}
-        renderItem={({ item }) => <BookCardMoreInfo book={item} navigation={navigation} />}
-        keyExtractor={(item, index) => 'key' + index}
-        onEndReached={handleLoadMore}
-        onEndReachedThreshold={0.5}
-      />
-    </View>
+      <View>
+        <SearchBar onSearch={handleSearch} onScan={() => navigation.navigate('Scanner')} />
+        <FlatList
+          data={books}
+          renderItem={({ item }) => <BookCardMoreInfo book={item} navigation={navigation} />}
+          keyExtractor={(item, index) => 'key' + index}
+          onEndReached={handleLoadMore}
+          onEndReachedThreshold={0.5}
+        />
+      </View>
     </SafeAreaView>
   );
 }
