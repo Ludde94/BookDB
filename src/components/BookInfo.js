4612934f3d89dc776c78d7fcadc6b0c1a4a054db
gnagetEmil <emil@gnaget.se>
2024-07-13 23:50:20 +0200

    Hid unknown information from book details screen

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -4,14 +4,14 @@ import styles from './styles/BookDetailInfoStyles';
 
 const BookDetailInfo = ({ book }) => (
     <>
-        <Text style={styles.detail}>Author: {book.authors}</Text>
-        <Text style={styles.detail}>Year: {book.publishedYear}</Text>
-        <Text style={styles.detail}>Publisher: {book.publisher}</Text>
-        <Text style={styles.detail}>Genre: {book.genre}</Text>
-        <Text style={styles.detail}>ISBN: {book.isbn}</Text>
-        <Text style={styles.description}>Pages: {book.pageCount}</Text>
-        <Text style={styles.description}>language: {book.language}</Text>
-        <Text style={styles.description}>Description: {book.description}</Text>
+        {book.authors && <Text style={styles.detail}>Author: {book.authors}</Text>}
+        {book.publishedYear && <Text style={styles.detail}>Year: {book.publishedYear}</Text>}
+        {book.publisher && <Text style={styles.detail}>Publisher: {book.publisher}</Text>}
+        {book.genre && <Text style={styles.detail}>Genre: {book.genre}</Text>}
+        {book.isbn && <Text style={styles.detail}>ISBN: {book.isbn}</Text>}
+        {book.pageCount && <Text style={styles.description}>Pages: {book.pageCount}</Text>}
+        {book.language && <Text style={styles.description}>language: {book.language}</Text>}
+        {book.description && <Text style={styles.description}>Description: {book.description}</Text>}
     </>
 );
 
