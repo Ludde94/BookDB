c27b3386a3130685ef297fa50a16609e28f3bcc4
gnagetEmil <emil@gnaget.se>
2024-07-13 02:07:13 +0200

    Added scanner.js file

---
+++ src/components/Scanner/Scanner.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+export default function Scanner() {
+  return (
+    <View style={styles.container}>
+     <Text>CAMERA</Text>
+    </View>
+  );
+}
