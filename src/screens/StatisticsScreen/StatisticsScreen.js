09deeb2185a9bdd04c339645a2d50bb884fe5f25
ludde94 <l.jenisch@hotmail.com>
2024-07-15 00:11:12 +0200

    updated styles to sep file

--- src/screens/StatisticsScreen/StatisticsScreen.js
+++ src/screens/StatisticsScreen/StatisticsScreen.js
@@ -4,6 +4,7 @@ import { PieChart } from 'react-native-chart-kit';
 import { fetchBooksFromLibrary, fetchBooksFromWantToRead } from '../../db/Storage.js';
 import colors from '../../themes.js';
 import { useFocusEffect } from '@react-navigation/native';
+import styles from './styles/StatisticsScreenStyles.js'; 
 
 const StatisticsScreen = () => {
   const [libraryBooks, setLibraryBooks] = useState([]);
@@ -115,37 +116,5 @@ const StatisticsScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  infoContainer: {
-    marginTop: 16,
-    alignItems: 'center',
-  },
-  infoText: {
-    fontSize: 18,  // Increased font size for info texts
-    color: colors.text,
-    marginVertical: 4,  // Added vertical margin for better spacing
-  },
-  AuthorText: {
-    fontSize: 16,  // Increased font size for info texts
-    color: colors.text,
-    marginVertical: 4,  // Added vertical margin for better spacing
-  },
-  subHeader: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-});
 
 export default StatisticsScreen;
---
+++ src/screens/StatisticsScreen/components/styles/StatisticsScreenStyles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../../themes.js';
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  infoContainer: {
+    marginTop: 16,
+    alignItems: 'center',
+  },
+  infoText: {
+    fontSize: 18,  // Increased font size for info texts
+    color: colors.text,
+    marginVertical: 4,  // Added vertical margin for better spacing
+  },
+  AuthorText: {
+    fontSize: 16,  // Increased font size for info texts
+    color: colors.text,
+    marginVertical: 4,  // Added vertical margin for better spacing
+  },
+  subHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+});
+
+export default styles;
