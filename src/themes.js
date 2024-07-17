3d3243c62d3613ec0d67877d0b40eaea8def8077
gnagetEmil <emil@gnaget.se>
2024-07-17 21:46:28 +0200

    Revert "merge"
    This reverts commit f85d39ec6ffb460c4c7b45d7fc33a315bfea049b.

--- src/themes.js
+++ src/themes.js
@@ -1,46 +1,10 @@
 const colors = {
-  primary: '#4B9CC4',
-  secondary: '#97ACDD',
+  primary: '#b75269',
+  secondary: '#ced69c',
   background: '#fbf6f7',
   text: '#15090b',
   subtleText: '#808080',
-  accent: '#6975CE',
+  accent: '#97c778',
 };
-
-//#469ED2
-//#7CC4ED
-//#4EB9F7
-
-//#4253CB
-//#828FE3
-//#5F6FE3
-
-//#469ED2
-//#7CC4ED
-//#4EB9F7
-
-//#4B9CC4
-//#97ACDD
-//#6975CE
-
-//#67AE8C
-//#9DAECB
-//#807FBB
-
-//#2A86E3
-//#9F78ED
-//#C061EA
-
-//#47C68F
-//#90CFDD
-//#68A1D0
-
-//#13FDD6
-//#70DBFD
-//#3EB7FD
-
-//#C83A11
-//#654883
-//#2F4858
 
 export default colors;
