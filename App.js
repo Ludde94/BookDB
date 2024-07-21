223b5e16dbbe6fd446e815dc35dec1f99699b216
gnagetEmil <emil@gnaget.se>
2024-07-21 14:32:38 +0200

    Add react-native-gesture-handler dependency and register root component in App.js

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Platform } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import * as NavigationBar from 'expo-navigation-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Navigation from './src/Navigation';
-import colors from './src/themes';
+import React, { useEffect } from "react";
+import { StyleSheet, View, Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Navigation from "./src/Navigation";
+import colors from "./src/themes";
+import { registerRootComponent } from "expo";
+
+registerRootComponent(App);
 
 export default function App() {
   useEffect(() => {
-    if (Platform.OS === 'android') {
+    if (Platform.OS === "android") {
       NavigationBar.setBackgroundColorAsync(colors.background);
-      NavigationBar.setButtonStyleAsync('dark');
+      NavigationBar.setButtonStyleAsync("dark");
     }
   }, []);
 
   return (
     <SafeAreaProvider>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor={colors.background}
-      />
+      <StatusBar barStyle="light-content" backgroundColor={colors.background} />
       <View style={styles.container}>
         <Navigation />
       </View>
